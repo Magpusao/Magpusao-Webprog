21422a650eeeb2a38541271671f7c7dec9877a9d
Zandra Magpusao <magpusaozandra@gmail.com>
2026-04-10 11:21:45 +0800

    lab-act1

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My React App!</h1>
+        <p>
+          Name: [Zandra Magpusao] <br />
+          Email: [magpusaozandra@gmail.com] <br />
+          Github: [https://github.com/Magpusao/Landingpage]
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App;
